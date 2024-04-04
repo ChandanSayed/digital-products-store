c9689f573dfe8482863c0a4d8c2042653eabeedd
chandansayed <hangzhou635@gmail.com>
2024-04-04 23:44:18 +0600

    Card added

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,46 @@
+import { Card, CardContent, CardDescription, CardHeader } from '@/components/ui/card';
+import db from '@/db/db';
 import React from 'react';
 
-const page = () => {
-  return <div>Admin</div>;
+const page = async () => {
+  async function getSellData() {
+    const data = await db.order.aggregate({
+      _sum: { priceInCents: true },
+      _count: true
+    });
+
+    return {
+      amount: (data._sum.priceInCents || 0) / 100,
+      numberOfSales: data._count
+    };
+  }
+  const sellData = await getSellData();
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <DashboardCard title="Sale" subtitle={sellData.amount} body={sellData.numberOfSales} />
+    </div>
+  );
 };
 
 export default page;
+
+type DashboardProps = {
+  title: string;
+  subtitle: number;
+  body: number;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardHeader>{title}</CardHeader>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
+}
