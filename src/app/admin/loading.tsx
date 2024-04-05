ac168bbf112bbe457f43ef08a381d31045143356
chandansayed <hangzhou635@gmail.com>
2024-04-06 00:37:20 +0600

    Frature: Admin page

---
+++ src/app/admin/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader2 } from 'lucide-react';
+
+function AdminLoading() {
+  return (
+    <div className="flex justify-center">
+      <Loader2 className="size-24 animate-spins" />
+    </div>
+  );
+}
+
+export default AdminLoading;
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardDescription, CardHeader } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import db from '@/db/db';
 import { formatCurrency, formatNumber } from '@/lib/formatters';
 import React from 'react';
@@ -6,31 +6,44 @@ import React from 'react';
 const page = async () => {
   async function getSellData() {
     const data = await db.order.aggregate({
-      _sum: { priceInCents: true },
+      _sum: { pricePainInCents: true },
       _count: true
     });
 
     return {
-      amount: (data._sum.priceInCents || 0) / 100,
+      amount: (data._sum.pricePainInCents || 0) / 100,
       numberOfSales: data._count
     };
   }
-
-  const [sellData, userData] = await Promise.all([getSellData(), getUserData()]);
 
   async function getUserData() {
     const [userCount, orderData] = await Promise.all([
       db.user.count(),
       db.order.aggregate({
-        _sum: { pricePaidInCents: true }
+        _sum: { pricePainInCents: true }
       })
     ]);
-    return { userCount, averageValuePerUser: userCount == 0 ? 0 : (orderData._sum.pricePaidInCents || 0) / userCount / 100 };
+    return {
+      userCount,
+      averageValuePerUser: userCount == 0 ? 0 : (orderData._sum.pricePainInCents || 0) / userCount / 100
+    };
   }
+
+  async function getProductData() {
+    const [inactiveCount, activeCount] = await Promise.all([db.product.count({ where: { isAvailableProductForPurchase: false } }), db.product.count({ where: { isAvailableProductForPurchase: true } })]);
+    return {
+      activeCount,
+      inactiveCount
+    };
+  }
+
+  const [sellData, userData, productData] = await Promise.all([getSellData(), getUserData(), getProductData()]);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       <DashboardCard title="Sale" subtitle={`${formatNumber(sellData.amount)} orders`} body={formatCurrency(sellData.numberOfSales)} />
+      <DashboardCard title="Customer" subtitle={`${formatNumber(userData.averageValuePerUser)} orders`} body={formatCurrency(userData.userCount)} />
+      <DashboardCard title="Active Product" subtitle={`${formatNumber(productData.inactiveCount)} inactive`} body={formatNumber(productData.activeCount)} />
     </div>
   );
 };
@@ -47,7 +60,7 @@ function DashboardCard({ title, subtitle, body }: DashboardProps) {
   return (
     <Card>
       <CardHeader>
-        <CardHeader>{title}</CardHeader>
+        <CardTitle>{title}</CardTitle>
         <CardDescription>{subtitle}</CardDescription>
       </CardHeader>
       <CardContent>
