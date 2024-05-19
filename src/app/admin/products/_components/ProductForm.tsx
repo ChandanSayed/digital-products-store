6dda508b0356756f834780be2e2b3f5d92420597
ChandanSayed <hangzhou635@gmail.com>
2024-05-19 20:53:29 +0600

    Feature: Download and edit

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -16,21 +16,13 @@ export function ProductForm({ product }: { product?: Product | null }) {
     product == null ? addProduct : updateProduct.bind(null, product.id),
     {}
   );
-  const [priceInCents, setPriceInCents] = useState<number>(
-    product?.priceInCents || 0
-  );
+  const [priceInCents, setPriceInCents] = useState<number>(product?.priceInCents || 0);
 
   return (
     <form action={action} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input
-          type="text"
-          id="name"
-          name="name"
-          required
-          defaultValue={product?.name || ""}
-        />
+        <Input type="text" id="name" name="name" required defaultValue={product?.name || ""} />
         {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
@@ -41,14 +33,10 @@ export function ProductForm({ product }: { product?: Product | null }) {
           name="priceInCents"
           required
           value={priceInCents}
-          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
+          onChange={e => setPriceInCents(Number(e.target.value))}
         />
-        <div className="text-muted-foreground">
-          {formatCurrency((priceInCents || 0) / 100)}
-        </div>
-        {error.priceInCents && (
-          <div className="text-destructive">{error.priceInCents}</div>
-        )}
+        <div className="text-muted-foreground">{formatCurrency((priceInCents || 0) / 100)}</div>
+        {error.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
@@ -58,28 +46,19 @@ export function ProductForm({ product }: { product?: Product | null }) {
           required
           defaultValue={product?.description}
         />
-        {error.description && (
-          <div className="text-destructive">{error.description}</div>
-        )}
+        {error.description && <div className="text-destructive">{error.description}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
         <Input type="file" id="file" name="file" required={product == null} />
-        {product != null && (
-          <div className="text-muted-foreground">{product.filePath}</div>
-        )}
+        {product != null && <div className="text-muted-foreground">{product.filePath}</div>}
         {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
         <Input type="file" id="image" name="image" required={product == null} />
         {product != null && (
-          <Image
-            src={product.imagePath}
-            height="400"
-            width="400"
-            alt="Product Image"
-          />
+          <Image src={product.imagePath} height="400" width="400" alt="Product Image" />
         )}
         {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
